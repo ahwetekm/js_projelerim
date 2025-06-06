7de7749747b471ac701fafb1592c964841ceee2a
ahwetekm <ahwetekm@gmail.com>
2025-06-06 17:19:51 +0000

    HTML ve CSS dosyaları yüklendi

---
+++ script.js
@@ -0,0 +1,45 @@
+// Butonu seçme
+var buton = document.getElementById("butonum");
+
+// Butona tıklandığında çalışacak fonksiyonu atama
+buton.onclick = function () {
+    // Input alanlarını seçme
+    var gir1 = document.getElementById("input1");
+    var gir2 = document.getElementById("input2");
+    var is = document.getElementById("islem")
+
+    // Input alanlarındaki değerleri alma
+    var sayi1 = gir1.value;
+    var sayi2 = gir2.value;
+    var islem = is.value;
+
+    // Değerleri float'a dönüştürme
+    sayi1 = parseFloat(sayi1);
+    sayi2 = parseFloat(sayi2);
+
+
+    // İşlemleri yapalım
+    if (islem === "+") {
+        var sonuc = sayi1 + sayi2;
+        console.log(sonuc);
+    }
+    else if (islem === "-") {
+        var sonuc = sayi1 - sayi2;
+        console.log(sonuc);
+    }
+    else if (islem === "*") {
+        var sonuc = sayi1 * sayi2;
+        console.log(sonuc);
+    }
+    else if (islem === "/") {
+        var sonuc = sayi1 / sayi2;
+        console.log(sonuc);
+    }
+
+    // Output alanını seçme
+    var sonucpar = document.getElementById("sonucburada")
+
+    // Sonucu output alanına yazdırma
+    sonucpar.textContent = "Sonuç: " + sonuc;
+
+};
